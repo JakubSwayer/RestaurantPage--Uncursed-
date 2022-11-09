cb5dc213a2cc795d6fa51f805644617e1a515c21
Jakub <Swayer@vp.pl>
2022-11-09 19:35:42 +0100

    Menu done

--- src/Menu.js
+++ src/Menu.js
@@ -1,15 +1,32 @@
 const Menu = () => {
 
+    let list = ["Green Bean Salad","Crunchy Ribbons","Traditional Miso Soup"];
+
     const div = document.createElement("div");
     div.id = "inside";
 
     const h1 = document.createElement("h1");
     h1.innerText = "Menu";
-    const  h2 = document.createElement("h2");
-    h2.innerText = "Try recipies we put all our hearts to create and cook";
+    h1.classList.add('menu-header');
+    const menuList = document.createElement("div");
+    menuList.classList.add('menu-list');
+
+    for (let i = 0; i < list.length; i++) {
+        let el_h2 = document.createElement('h2');
+        el_h2.innerText = list[i];
+        let el_p = document.createElement('p');
+        el_p.innerText = "Egestas congue quisque egestas diam in arcu cursus. Venenatis tellus in metus vulputate eu scelerisque felis. Mauris a diam maecenas sed enim ut sem viverra.";
+        let el_hr = document.createElement('hr');
+
+        menuList.appendChild(el_h2);
+        menuList.appendChild(el_p);
+        menuList.appendChild(el_hr);
+    }
+
+
 
     div.appendChild(h1);
-    div.appendChild(h2);
+    div.appendChild(menuList);
 
     return(div);
 }
